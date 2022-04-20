6c001ed9b476fd91ae558c2640391d06514f9e5d
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-04-19 20:20:05 -0600

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,14 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import home, mol_vis, reactividad, parametros
 
+app = MultiApp()
+
+st.markdown("" "")
+
+app.add_app("Inicio",home.app)
+app.add_app("Visualización molecular",mol_vis.app)
+app.add_app("Calculo de parámetros de reactividad",reactividad.app)
+app.add_app("Otras propiedades",parametros.app)
+
+app.run()
