461f224fd33c47532525c14ffa61c46bfe10e3ed
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-04-25 11:18:47 -0600

    AASDF

--- app.py
+++ app.py
@@ -118,19 +118,21 @@ if seleccion == "Visualizacion molecular":
 
 
     if seleccion_molecula == "Subir un archivo":
-      uploaded_files = st.sidebar.file_uploader("Choose xyz files", accept_multiple_files=True)
-
-      for uploaded_file in uploaded_files:
-        xyz = uploaded_file.getvalue().decode("utf-8")
-        render_mol(xyz)
-
       def render_mol(xyz):
         xyzview = py3Dmol.view(width=400,height=400)
         xyzview.addModel(xyz,'xyz')
         xyzview.setStyle({'stick':{}})
         xyzview.setBackgroundColor('white')#('0xeeeeee')
         xyzview.zoomTo()
-        showmol(xyzview, height = 500,width=800)
+        showmol(xyzview, height = 500,width=800)      
+      
+      uploaded_files = st.sidebar.file_uploader("Choose xyz files", accept_multiple_files=True)
+
+      for uploaded_file in uploaded_files:
+        xyz = uploaded_file.getvalue().decode("utf-8")
+        render_mol(xyz)
+
+
 
     if seleccion_molecula == "SMILES":
       compound_smiles=st.text_input('SMILES please','COc1cccc2cc(C(=O)NCCCCN3CCN(c4cccc5nccnc54)CC3)oc21')
