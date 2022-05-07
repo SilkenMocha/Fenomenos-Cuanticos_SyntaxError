f94f60b03775b18da0b2c123c2f68913e80dbde1
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-05-07 11:45:02 -0600

    Update

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ from rdkit.Chem.Pharm2D import Gobbi_Pharm2D,Generate
 st.title ("FENÓMENOS CUÁNTICOS")
 st.subheader("Calculos de reactividad y visualización molecular")
 st.image("https://www.meteorologiaenred.com/wp-content/uploads/2021/12/que-es-la-fisica-cuantica-caracteristicas.jpg")
-st.subheader("Erick López Saldviar 348916")
+st.subheader("Erick López Saldviar — SyntaxError")
 
 
 seleccion = st.selectbox("Seleccione una opción: ", ["Visualizacion molecular", "Reactividad"])
